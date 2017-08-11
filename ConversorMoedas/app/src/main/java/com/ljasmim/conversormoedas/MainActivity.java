aed04cfda12b6248d01aaba94e0897815cc97e50
LeoJasmim <leojasmim@hotmail.com>
2017-08-11 16:49:02 -0300

    Aplicativo criado

---
+++ ConversorMoedas/app/src/main/java/com/ljasmim/conversormoedas/MainActivity.java
@@ -0,0 +1,56 @@
+package com.ljasmim.conversormoedas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = (EditText) findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id) {
+            case R.id.button_calculate:
+                String stringRealValue = this.mViewHolder.editValue.getText().toString();
+                if (!stringRealValue.isEmpty()) {
+                    Double realValue = Double.valueOf(stringRealValue);
+                    this.mViewHolder.textDollar.setText(String.format("US$ %.2f", realValue * 3.1604));
+                    this.mViewHolder.textEuro.setText(String.format("\u20ac$ %.2f", realValue * 3.7413));
+                }
+        }
+    }
+
+    private void clearValues() {
+        this.mViewHolder.textDollar.setText("");
+        this.mViewHolder.textEuro.setText("");
+    }
+
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
+    }
+}
